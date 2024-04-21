9933122b3e813a90e31056854415b24c5f685ade
Nicole Nguyen <94734946+nicolvn1@users.noreply.github.com>
2024-04-20 18:46:57 -0700

    Update extract new links

--- scraper.py
+++ scraper.py
@@ -15,7 +15,16 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    links = []
+    soup = BeautifulSoup(resp.content, 'html5lib')
+    for s in soup(["script", "style"]):
+        s.extract()
+    # text = soup.get_text()
+    # print(text)
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
