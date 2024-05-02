2dd1296a77b4846ff845d6be2a792c75de57e994
Yuuuouhan <57851400+Yuuuouhan@users.noreply.github.com>
2024-05-01 22:25:07 -0700

    Update worker.py to add/change various functions

--- crawler/worker.py
+++ crawler/worker.py
@@ -17,68 +17,44 @@ class Worker(Thread):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+        self.counter = 0
+        self.stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+        self.unique_pages = set()
+        self.max_len = 0
+        self.max_url = ""
+        self.freq = {}
+        self.ics_subdomains = {}
+        self.ics_subdomains_formatted = []
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
         
     def run(self):
-        stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
-        unique_pages = set()
-        max_len = 0
-        max_url = ""
-        freq = {}
-        ics_subdomains = {}
-        ics_subdomains_formatted = []
-        
+        # runs the thing
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
             """
-            # Roboto.txt commented because not really working
-            # check robot.txt
-            can_crawl, delay = self.checkRobotTxt(tbd_url)
-            if not can_crawl:
-                print("ROBOT NOT CRAWLABLE")
-                self.frontier.mark_url_complete(tbd_url)
-                time.sleep(self.config.time_delay)
-                continue
-            """
-            """
-            # HEAD-related commented because not really working...
-            # REQUEST HEAD FOR STATUS CODE, REDIRECT, AND FILE SIZE
-            head = download_header(tbd_url, self.config, self.logger)
-            if not str(head).startswith("<Response [2") or not str(head).startswith("<Response [3"):
-                print("HEAD IS NOT STATUS 200 or 300")
-                self.frontier.mark_url_complete(tbd_url)
-                time.sleep(self.config.time_delay)
-                continue
-            
-            # find redirect as long as returned is different
-            redirect = self.headerRedirect(tbd_url, head)
-            while redirect != tbd_url:
-                head = download_header(tbd_url, self.config, self.logger)
-                redirect = self.headerRedirect(tbd_url, head)
-                print("REDIRECTING FROM HEAD")
-            # check if reported length is too big, if so, skip file
-            if "Content-Length" in head.headers and int(head.headers["Content-Length"]) > 1048576:
-                print("SKIP SIZE FROM HEAD")
-                self.frontier.mark_url_complete(tbd_url)
-                time.sleep(self.config.time_delay)
-                continue
+            # Header checking
+            header = download_header(tbd_url)
+            if header:
+                # check header for redirect
+                header_redirect = self.check_redirect_header(header, tbd_url)
+            FIX
             """
             
             resp = download(tbd_url, self.config, self.logger)
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            """
-            if delay > self.config.time_delay:
-                print(f"COOLDOWN: {self.config.time_delay - delay}")
-                time.sleep(delay - self.config.time_delay)
-            """
+            # to print out result every 100 downloads
+            self.count += 1
+            if self.count == 100:
+                self.count = 0
+                # print out results
             # Check if status is 2xx
             if resp.status // 100 != 2:
                 print("STATUS NOT 2xx")
@@ -207,22 +183,45 @@ class Worker(Thread):
             f"The longest page {max_url} has {max_len} words. "
             f"Top 50 most common words: {result[0:50]}. "
             f"All ics.uci.edu subdomains: {ics_subdomains_formatted}")
+
+    def calcData(self, resp):
+        # calculates the word count of content
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        # Remove the script and style elements of the page
+        for s in soup(["script", "style"]):
+            s.extract()
+        # Create a list of words composed of alphanumeric characters and apostrophes
+        words = [word for word in re.split("[^a-zA-Z0-9-']", soup.get_text()) if word != ""]
+        # check if this is the longest page so far
+        if len(words) > self.max_len:
+            self.max_len = len(words)
+            self.max_url = resp.url
+        # Add frequency of words to class dict of words
+        for k in words:
+            k = k.lower()
+            if len(k) > 1 and k not in self.stopwords:
+                if k not in self.freq:
+                    self.freq[k] = 1
+                else:
+                    self.freq[k] += 1
+        # Check the subdomains
+        if ".ics.uci.edu" in resp.url:
+            subdomain = resp.url.split(".ics.uci.edu")[0] + ".ics.uci.edu"
+            if subdomain not in self.ics_subdomains:
+                self.ics_subdomains[subdomain] = 1
+            else:
+                self.ics_subdomains[subdomain] += 1
+        return len(words)
     
-    def checkNoIndex(self, soup):
+    def checkCrawlable(self, soup):
+        # check if web is crawlable or readable
+        xmlExist = soup.find("xml")
         noIndex = soup.find("meta", content="noindex")
         noFollow = soup.find("meta", content="nofollow")
         noNeither = soup.find("meta", content="noindex,nofollow")
-        if noIndex or noFollow or noNeither:
-            return True
-    
-    def checkRedirect(self, soup):
-        # FOR META TAG ONLY
-        refreshes = soup.find("meta", http_equiv="refresh")
-        redirect = refreshes["content"] if refreshes else None
-        if redirect is not None and ";" in redirect:
-            redir_url = redirect.split(";")[-1]
-            return redir_url
-        return None
+        if noIndex or noFollow or noNeither or xmlExist:
+            return False
+        return True
     
     def headerRedirect(self, url, head):
         # request header only to check FOR REDIRECT
@@ -245,8 +244,37 @@ class Worker(Thread):
         else:
             return url   
 
+    def checkLengthHeader(self, head):
+        # check the header to see if file too big
+        if "Content-Length" in head.headers:
+            if int(head.headers["Content-Length"]) > 1048576:
+                return False
+        return True
+
+    def checkRedirect(self, soup):
+        # various redirect checking
+        # check canonical
+        canonical = self.checkCanonical(soup)
+        if canonical:
+            return canonical
+        # check refresh
+        refresh = self.checkRefresh(soup)
+        if refresh:
+            return refresh
+        return None
+    
+    def checkRefresh(self, soup):
+        # check for refresh link
+        refreshes = soup.find("meta", http_equiv="refresh")
+        redirect = refreshes["content"] if refreshes else None
+        if redirect:
+            if ";" in redirect:
+                return redirect.split(";")[0]
+            return redir_url
+        return None
+    
     def checkCanonical(self, soup):
-        # check for canonical web, which is a duplicate of current
+        # check for canonical web, which points to the main website that it should be, which might be duplicated
         # first check link rel
         rel = soup.find("link", rel="canonical")
         canonical = rel["href"] if rel else None
@@ -257,25 +285,24 @@ class Worker(Thread):
         canonical = og_url["content"] if og_url else None
         return canonical
 
-    """
-    # Commented out because not really working
-    def checkRobotTxt(self, url):
-        # check robot.txt to see if web is crawlable
-        # returns tuple of (Bool, delay-time)
-        rp = urllib.robotparser.RobotFileParser()
-        # parse link to only fed scheme and domain to the robot parser
-        link = urlparse(url)
-        # invalid link
-        if not link.scheme or not link.netloc:
-            return (False, 0)
-        robot_link = link.scheme + "://" + link.netloc
-        robot_link = urljoin(robot_link, "robot.txt")
-        rp.set_url(robot_link)
-        try:
-            rp.read()
-            delay = rp.crawl_delay('*') 
-            delay = delay if delay else 0
-            return (rp.can_fetch('*', url), delay)
-        except Exception as e:
-            return (False, 0)
-        """
+    def check_same_url(self, url1, url2):
+        # check two urls regardless of scheme and www.
+        parsed1 = urlparse(url1)
+        parsed2 = urlparse(url2)
+        netloc1 = parsed1.netloc.strip("www.")
+        netloc2 = parsed2.netloc.strip("www.")
+        return (netloc1 == netloc2 and parsed1.path == parsed2.path and parsed1.query == parsed2.query
+                and parsed1.param == parsed2.param)
+
+    def final_results(self):
+        # print all 4 questions
+        for subdomain in sorted(self.ics_subdomains.keys()):
+            self.ics_subdomains_formatted.append(f"{subdomain}, {self.ics_subdomains[subdomain]}")
+        result = sorted(self.freq.items(), key = lambda x: (-x[1], x[0]))
+        self.logger.info(
+            f"{len(self.unique_pages)} total unique urls discovered. "
+            f"The longest page {self.max_url} has {self.max_len} words. "
+            f"Top 50 most common words: {result[0:50]}. "
+            f"All ics.uci.edu subdomains: {self.ics_subdomains_formatted}")
+        
+        
